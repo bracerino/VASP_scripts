ee66fb1dc81250c2073ad82706fce7829ca2a7e9
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2024-02-21 12:06:39 +0100

    Update create_poscars.sh

--- create_poscars.sh
+++ create_poscars.sh
@@ -1,14 +1,23 @@
 # Base directory containing the original files
-base_dir="."
-# Target directory where new directories and files will be created
-target_dir="${base_dir}/POSCARs"
+directory_path="/home/lebedmi2/DATA/VASP_data/Ti_test_structures/scripts/structures"
 
+target_dir="${directory_path}/POSCARs"
+# Ensure the target directory exists
 mkdir -p "$target_dir"
-for file in "$base_dir"/*; do
+
+# Loop over each file in the base directory
+for file in "$directory_path"/*; do
     if [ -f "$file" ]; then
+        # Extract the full filename without the path
         filename=$(basename -- "$file")
+
+        # Create a directory with the name of the file inside the target directory
         dir_name="$target_dir/$filename"
+
+        # Ensure the directory exists
         mkdir -p "$dir_name"
+
+        # Move and rename the file to 'POSCAR' within the new directory
         cp "$file" "$dir_name/POSCAR"
         cp "./INCAR" "$dir_name/"
         cp "./KPOINTS" "$dir_name/"
